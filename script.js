ed59c5c3e6877648fba859defeef107765bcfd65
jacu <jacu@ailleron.com>
2023-01-05 20:46:45 +0100

    + Temp correct data schema file instead of random api + top 10 button

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ const mainBtn = document.querySelector(".main-btn")
 mainBtn.addEventListener('click', function() {
 })
 
+var button = document.getElementById("displayButton");
+button.addEventListener("click", showTenPlayers);
+
 const API_URL = 'https://my.api.mockaroo.com/edi.json?key=953a0b00';
-//Na ten moment działa na innym api bo tam w dniu commitowania się wyczerpały requesty, a chce żeby coś się wyświetlało, jutro 06.01 jak odświeży się pula requestów to zmienię kod na dane pod tamten schemat//
+//Na ten moment działa na innym api bo w dniu commitowania się wyczerpały requesty, a chce żeby coś się wyświetlało, jutro 06.01 jak odświeży się pula requestów to zmienię kod na dane pod tamten schemat//
 const test_url = 'https://random-data-api.com/api/v2/users?size=2&is_json=true';
 
-fetch(test_url)
+fetch("EDI.json")
     .then(response => response.json())
     .then(data => {
       console.log(data);
       const playerArray = document.getElementById('player-array');
       data.forEach(item => {
       const row = document.createElement('tr');
-      row.innerHTML = `<td>${item.id}</td><td>${item.uid}</td><td>${item.zip_code}</td><td>${item.social_insurance_number}</td><td>${item.username}</td><td>${item.first_name}</td><td>${item.last_name}</td><td>${item.date_of_birth}</td>`
+      row.innerHTML = `<td>${item.id}</td><td>${item.name}</td><td>${item.score}</td><td>${item.rank}</td><td>${item.country}</td><td>${item.email}</td><td>${item.ip_address}</td><td>${item.registered}</td>`
       playerArray.appendChild(row);
       })
     });
+
+function showTenPlayers(){
+  var tab = document.getElementById("player-array")
+  var numberOfRows = tab.rows.length;
+  for (var i = 11; i < numberOfRows; i++) {
+    tab.rows[i].style.display = "none";
+  }
+}
