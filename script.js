504ec7efcb12e22ab3d4c5a810e45cec767546bc
ceculajakub <ceculajakub@gmail.com>
2023-01-05 01:15:34 +0100

    test

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const mainBtn = document.querySelector(".main-btn")
 mainBtn.addEventListener('click', function() {
     mainBtn.style.backgroundColor = '#cdd3ff';
 })
+
+const API_URL = 'https://my.api.mockaroo.com/edi.json?key=953a0b00';
+
+fetch(API_URL)
+  .then(response => response.json())
+  .then(data => {
+    // const playerArray = document.getElementById('player-array');
+
+    // data.players.forEach(player => {
+    //   const div = document.createElement('div');
+    //   div.textContent = JSON.stringify(player);
+    //   playerArray.appendChild(div);
+    console.log(data);
+    });
