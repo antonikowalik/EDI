b20e335e5720f20f24580f4688650412125f1098
ceculajakub <ceculajakub@gmail.com>
2023-01-06 14:38:00 +0100

    + merge refactor

--- script.js
+++ script.js
@@ -4,21 +4,6 @@ mainBtn.addEventListener('click', function() {
     mainBtn.style.backgroundColor = '#cdd3ff';
 })
 
-const API_URL = 'https://my.api.mockaroo.com/edi.json?key=953a0b00';
-
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
-    // const playerArray = document.getElementById('player-array');
-
-    // data.players.forEach(player => {
-    //   const div = document.createElement('div');
-    //   div.textContent = JSON.stringify(player);
-    //   playerArray.appendChild(div);
-    console.log(data);
-    });
-})
-
 var button = document.getElementById("displayButton");
 button.addEventListener("click", showTenPlayers);
 
